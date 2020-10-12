176240f952e0dee309cd6c969f0fdaba62dd09d7
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2020-10-12 17:26:41 +0200

    fit adding blog creation

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,13 +1,15 @@
         // Your web app's Firebase configuration
         var firebaseConfig = {
-            apiKey: "AIzaSyDJp5pwKuLG4rgxlxNaiLrDLzRtSMBi8CY ",
-            authDomain: "demoproject-41275.firebaseapp.com ",
-            databaseURL: "https://demoproject-41275.firebaseio.com ",
-            projectId: "demoproject-41275 ",
-            storageBucket: "demoproject-41275.appspot.com ",
-            messagingSenderId: "805951828018 ",
-            appId: "1:805951828018:web:8c5e199c2bd165810d4c2b "
+            apiKey: "AIzaSyDJp5pwKuLG4rgxlxNaiLrDLzRtSMBi8CY",
+            authDomain: "demoproject-41275.firebaseapp.com",
+            databaseURL: "https://demoproject-41275.firebaseio.com",
+            projectId: "demoproject-41275",
+            storageBucket: "demoproject-41275.appspot.com",
+            messagingSenderId: "805951828018",
+            appId: "1:805951828018:web:8c5e199c2bd165810d4c2b"
         };
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
         const db = firebase.firestore();
+        let auth = firebase.auth();
+        let storage = firebase.storage();
